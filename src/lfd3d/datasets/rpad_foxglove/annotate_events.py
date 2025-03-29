945e79adbe69d5ff7546b986e07ac5b1ad34280e
Sriram Krishna <sriramsk1999@gmail.com>
2025-03-29 00:33:04 -0400

    annotate robot demos with gripper pos

--- src/lfd3d/datasets/rpad_foxglove/annotate_events.py
+++ src/lfd3d/datasets/rpad_foxglove/annotate_events.py
@@ -99,6 +99,58 @@ def save_event_images(output_dir, images, ts, event_ts, goals):
         plt.imsave(f"{output_dir}/{goal}.png", images[idx])
 
 
+def extract_events_with_gemini(
+    images,
+    ts,
+    video_path,
+    client,
+    model_name,
+    generate_content_config,
+    goal_text,
+    subgoals,
+):
+    fps = save_video(images, video_path, approx_duration=30)
+    prompt = generate_prompt_with_subgoals(goal_text, subgoals)
+    parsed_json = process_with_gemini(
+        client, model_name, generate_content_config, video_path, prompt
+    )
+    ends, events = [], []
+    for item in parsed_json:
+        end_sec = float(item["timestamp"][3:])
+        end_idx = int(end_sec * fps)
+        end_ts = ts[end_idx]
+
+        goal = item["subgoal"]
+        end = datetime.fromtimestamp(end_ts).isoformat()
+
+        events.append(goal)
+        ends.append(end)
+    return ends, events
+
+
+def extract_events_with_gripper_pos(
+    joint_states, subgoals, close_thresh=-0.4, open_thresh=0
+):
+    """
+    First event ends when gripper closes,
+    second events ends when gripper opens again.
+    """
+    assert len(subgoals) == 2, "designed only for 2 subgoals"
+    joint_ts = np.asarray(joint_states["publish_ts"])
+    gripper_pos = np.asarray(joint_states["pos"])[:, 6]
+
+    close_gripper = np.where(gripper_pos < close_thresh)[0][0]
+    open_gripper = (
+        close_gripper + np.where(gripper_pos[close_gripper:] > open_thresh)[0][0]
+    )
+
+    ends = [
+        datetime.fromtimestamp(joint_ts[i]).isoformat()
+        for i in [close_gripper, open_gripper]
+    ]
+    return ends, subgoals
+
+
 def main(args):
     """Main function to process the dataset and generate subgoal timestamps."""
     client = setup_client(os.environ.get("RPAD_GEMINI_API_KEY"))
@@ -116,34 +168,38 @@ def main(args):
         response_mime_type="text/plain",
     )
 
-    for demo_name in tqdm(dataset):
+    pbar = tqdm(dataset)
+    for demo_name in pbar:
+        pbar.set_description(demo_name)
         demo = dataset[demo_name]
         os.makedirs(f"{args.output_dir}/{demo_name}", exist_ok=True)
         video_path = f"{args.output_dir}/{demo_name}/video.mp4"
 
-        images = np.asarray(demo["_rgb_image_rect"]["img"])
         ts = np.asarray(demo["_rgb_image_rect"]["publish_ts"])
-        fps = save_video(images, video_path, approx_duration=30)
+        images = demo["_rgb_image_rect"]["img"]
 
-        prompt = generate_prompt_with_subgoals(goal_text, subgoals)
-        parsed_json = process_with_gemini(
-            client, model_name, generate_content_config, video_path, prompt
-        )
+        if "_follower_right_joint_states" in demo and not args.disable_gripper_pos:
+            joint_states = demo["_follower_right_joint_states"]
+            ends, events = extract_events_with_gripper_pos(joint_states, subgoals)
+        else:
+            ends, events = extract_events_with_gemini(
+                images,
+                ts,
+                video_path,
+                client,
+                model_name,
+                generate_content_config,
+                goal_text,
+                subgoals,
+            )
 
-        ends, events = [], []
-        for item in parsed_json:
-            end_sec = float(item["timestamp"][3:])
-            end_idx = int(end_sec * fps)
-            end_ts = ts[end_idx]
+        if "events" in demo:
+            if len(demo["events"]["event"]) == 0:
+                del demo["events"]
+            else:
+                print("skipping as events are already present")
+                continue
 
-            goal = item["subgoal"]
-            end = datetime.fromtimestamp(end_ts).isoformat()
-
-            events.append(goal)
-            ends.append(end)
-
-        if "events" in demo and len(demo["events"]["event"]) == 0:
-            del demo["events"]
         events_group = demo.create_group("events")
         ends = np.array(ends)
         events = np.array(events)
@@ -164,6 +220,12 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--task_spec", default="place_mug_on_table", help="Task specified in dataset"
+    )
+    parser.add_argument(
+        "--disable_gripper_pos",
+        default=False,
+        action="store_true",
+        help="For robot demos, by default we use the gripper state to segment demos. With this arg, we still use Gemini.",
     )
     parser.add_argument(
         "--model_name",
