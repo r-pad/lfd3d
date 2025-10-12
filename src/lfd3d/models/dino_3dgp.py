0a7ab629b45301efb354725bb27dd715daab503b
Haotian <hz2851@nyu.edu>
2025-10-12 00:09:17 -0400

    0 -> 0.0

--- src/lfd3d/models/dino_3dgp.py
+++ src/lfd3d/models/dino_3dgp.py
@@ -596,8 +596,9 @@ class Dino3DGPGoalRegressionModule(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         """Training step with 3D GMM prediction"""
+        breakpoint()
         assert (
-            batch["augment_t"].mean().item() == 0
+            batch["augment_t"].mean().item() == 0.0
         ), "Disable pcd augmentations when training image model!"
 
         self.train()
